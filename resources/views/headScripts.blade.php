d34a61d77f8531f2e50a897ad540280cbdd18228
SterbenMy <cristian.macari.info@gmail.com>
2021-05-12 14:09:21 +0300

    added head scripts page

---
+++ resources/views/headScripts.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="{{ mix("css/bootstrap.css") }}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ mix("css/all.css") }}">
+
+
