d63d07812473812b741ed1d37e4be56b70943fe4
SterbenMy <cristian.macari.info@gmail.com>
2021-05-12 14:09:07 +0300

    added header page

---
+++ resources/views/header.blade.php
@@ -0,0 +1,46 @@
+<header>
+    <div class="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="social-media">
+                        <ul>
+                            <li><a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://twitter.com"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="https://www.linkedin.com"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">
+            <img src="{{asset('img/logo.png')}}" width="100" height="100" alt="logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse nav1" id="navbarNav">
+            <ul class="navbar-nav nav1 ">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{'/'}}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{'/'}}">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{'/'}}">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{'/'}}">Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ab" href="{{'/'}}">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
