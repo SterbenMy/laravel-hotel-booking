00ede8582ec56adf11cdb74cbb378bfb46d97db8
SterbenMy <cristian.macari.info@gmail.com>
2021-05-14 21:41:15 +0300

    auto changes

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\ArticleTag;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 
 class ArticleController extends Controller
 {
