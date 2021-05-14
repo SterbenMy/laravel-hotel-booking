e02dc1e5e13b272d19e0b0e5603792527097ad2f
SterbenMy <cristian.macari.info@gmail.com>
2021-05-14 21:39:28 +0300

    added contact(email) route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactFormController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,5 @@ Route::get('/',[HomeController::class,'index']);
 Route::get('article/{id}',[ArticleController::class,'index']);
 Route::get('/category/{id}',[CategoryController::class,'index']);
 Route::get('/tag/{id}',[TagController::class,'index']);
+Route::get('/email', [ContactFormController::class,'index']);
+Route::post('/email', [ContactFormController::class,'sendMailas']);
