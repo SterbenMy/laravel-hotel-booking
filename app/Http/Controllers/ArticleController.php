fd5d71747b901bc6d77e3a9d440c203253e428fd
SterbenMy <cristian.macari.info@gmail.com>
2021-07-21 12:56:40 +0300

    added showComments and store method

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\ArticleComment;
 use App\Models\ArticleTag;
+use Illuminate\Http\Request;
 
 
 class ArticleController extends Controller
@@ -24,8 +26,12 @@ class ArticleController extends Controller
             ->orderBy('articles.create_at', 'desc')
             ->find($id);
 
+        $comments = $this->showComments($id);
         $tags = $this->showTag($id);
-        return view('pages.article', compact('article'), compact('tags'));
+        return view('pages.article')
+            ->with(compact('article'))
+            ->with(compact('tags'))
+            ->with(compact('comments'));
     }
 
     public function showTag($id)
@@ -44,5 +50,32 @@ class ArticleController extends Controller
                 $join->on('tg.article_id', "=", 'articles.id');
             })->where('articles.id', "=", $id)->get();
         return $tags;
+    }
+
+    public function showComments($id)
+    {
+        $comments =
+            ArticleComment::select('message', 'updated_at', 'author')->where('article_id', "=", $id)->paginate(3);
+        return $comments;
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'message' => 'bail|required|min:3|max:255',
+            'author' => 'bail|required'
+        ], [
+            'author.required'=> 'A author is required',
+            'message.required' => 'A message is required',
+            'message.min' => 'The message must be at least 3 characters long',
+            'message.max' => 'The message cannot exceed 255 characters'
+        ]);
+
+        ArticleComment::create([
+            'message' => $request->input('message'),
+            'article_id' => $request->input('article_id'),
+            'author' => $request->input('author')
+        ]);
+        return back()->with(['message' => 'Article successfully added!']);
     }
 }
