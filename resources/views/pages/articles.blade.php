83d62bf609fd5221c82913a24cf9171f17e91e6c
SterbenMy <cristian.macari.info@gmail.com>
2021-07-21 12:51:31 +0300

    renamed to articles

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -19,7 +19,7 @@
                     <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item">
-                                <a href="#">Home</a>
+                                <a href="/">Home</a>
                             </li>
                             <li class="breadcrumb-item active" aria-current="page">
                                 Articles
@@ -34,13 +34,14 @@
         <div class="container">
             <div class="row">
                 @foreach($articles as $article)
-                <div class="col-sm-4 padd">
-                    <div class="card mb-3">
+                <div class="col-lg-4 padd">
+                    <div class="card h-100 mb-3">
                         <img src="{{asset('img/'.$article->path)}}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{$article->title}}</h5>
                             <p class="card-text">{{$article->short_description}}</p>
                             <a href="{{"article/".$article->id}}" class="btn btn-primary">Read Article</a>
+{{--                            <add-cart-component :product-id="{{json_encode($article->id)}}" :product-name="{{json_encode($article->title)}}" :product-description="{{json_encode($article->short_description)}}"></add-cart-component>--}}
                             <p class="card-text"><small class="text-muted "> Created {{$article->create_at}}</small></p>
                         </div>
                     </div>
