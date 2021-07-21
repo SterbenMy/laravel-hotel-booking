04ee40210df79b96fdada8d6ec0ea522dbc019aa
SterbenMy <cristian.macari.info@gmail.com>
2021-07-21 12:59:42 +0300

    removed comments

--- resources/views/paginator.blade.php
+++ resources/views/paginator.blade.php
@@ -1,20 +1,19 @@
 @if($paginator->hasPages())
     <ul class="pagination">
-        <!-- Prevoius Page Link -->
         @if($paginator->onFirstPage())
             <li class="page-item disabled"><a class="page-link"><span>&laquo;</span></a></li>
         @else
             <li class="page-item"><a href="{{ $paginator->previousPageUrl() }}" class="page-link" rel="prev">&laquo;</a></li>
         @endif
 
-    <!-- Pagination Elements Here -->
+
         @foreach($elements as $element)
-        <!-- Make three dots -->
+
             @if(is_string($element))
                 <li class="page-item disabled"><a  class="page-link"><span>{{$element}}</span></a></li>
             @endif
 
-        <!-- Links Array Here -->
+
             @if(is_array($element))
                 @foreach($element as $page=>$url)
 
@@ -29,7 +28,7 @@
 
         @endforeach
 
-    <!-- Next Page Link -->
+
         @if($paginator->hasMorePages())
             <li class="page-item"><a href="{{ $paginator->nextPageUrl() }}" class="page-link"><span>&raquo;</span></a></li>
         @else
